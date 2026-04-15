1f631dd81ac57f9ae84daf9af0aa8011f2236750
abdulana1 <anab.abdulla44@gmail.com>
2026-04-15 15:08:52 +0200

    CC Lektion 7-frontend

--- hotel-frontend/script.js
+++ hotel-frontend/script.js
@@ -1,6 +1,38 @@
 //const apiUrl = "https://wdb26-exempel-deployment-testing.2.rahtiapp.fi/api/ip";
 const apiUrl = "http://127.0.0.1:8080";
 
+async function getBookings() {
+    const res = await fetch(`${apiUrl}/bookings`);
+    const bookings = await res.json();
+
+    document.getElementById("bookings-list").innerHTML = "";
+
+    for (const b of bookings) {
+        document.getElementById("bookings-list").innerHTML += `
+            <li>${b.firstname} ${b.lastname} – rum ${b.room_number} – ${b.datefrom} → ${b.dateto}</li>
+        `;
+    }
+}
+getBookings();
+
+getRooms();
+
+async function getGuests() {
+    const res = await fetch(`${apiUrl}/guests`);
+    const guests = await res.json();
+
+    for (const g of guests) {
+        document.getElementById("guest-list").innerHTML += `
+            <option value="${g.id}">
+                ${g.firstname} ${g.lastname} (${g.total_visits} besök)
+            </option>
+        `;
+    }
+}
+getGuests();
+
+
+
 async function getRooms() {
     const res = await fetch(`${apiUrl}/rooms`);
     const rooms = await res.json();
@@ -18,13 +50,12 @@ async function getRooms() {
     }
     
 }
-getRooms();
 
 async function saveBooking() {
 
     const booking = {
         room_id: document.getElementById("room-list").value,
-        guest_id: 1, // TEMP hardcoded
+        guest_id: document.getElementById("guest-list").value,
         datefrom: document.getElementById("datefrom").value,
         dateto: document.getElementById("dateto").value
     }
